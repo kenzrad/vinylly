cdb263bb72e41607ea46928bb31ab40852d604f3
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-03-30 14:05:19 -0400

    css and html adjustments made for record room

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,5 +3,12 @@
 $("#record-img").attr("style", "visibility: hide");
 $("#album-img").click(function() {
     // $('#record-div').slideToggle("slide");
-    $('#record-div').
+    $('#record-div').animate({
+        width: "toggle"
+    })
 });
+
+//Need to figure out what this actually does
+// $('.modal-btn').on('shown.bs.modal', function () {
+//     $('#myInput').trigger('focus')
+//   })

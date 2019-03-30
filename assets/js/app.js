4b370a2e02e397ad3f3db76c029dec9ccb470459
barkgnar <45748185+kenzrad@users.noreply.github.com>
2019-03-30 11:29:10 -0400

    slide toggle added

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+
+////TOGGLE ALBUM RECORD////
+$("#record-img").attr("style", "visibility: hide");
+$("#album-img").click(function() {
+    // $('#record-div').slideToggle("slide");
+    $('#record-div').
+});
